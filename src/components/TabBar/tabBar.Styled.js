7aa29eac6418a7e2ee12f3ba90000e8243178991
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-01-06 15:34:45 -0300

    styleding Icons tabBar

--- src/components/TabBar/tabBar.Styled.js
+++ src/components/TabBar/tabBar.Styled.js
@@ -1,31 +1,32 @@
 import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
-  background-color: #000;
+  background-color: #333333;
   height: 60px;
   margin-top: 113%;
   width: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: flex-end;
-  justify-content: center;
   justify-content: space-evenly;
-`;
-
-export const ContactButton = styled.TouchableOpacity`
-  height: 50px;
-  width: 50px;
-  background: red;
-  justify-content: flex-end;
-  align-items: flex-end;
-  border-radius: 30px;
 `;
 
 export const PostList = styled.TouchableOpacity`
   height: 50px;
   width: 50px;
-  background: red;
-  justify-content: flex-end;
-  align-items: flex-end;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
   border-radius: 30px;
+  border: 2px #F1A10A;
+`;
+
+export const ContactButton = styled.TouchableOpacity`
+  height: 50px;
+  width: 50px;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  border: 2px #F1A10A;
 `;
--- src/components/TabBar/tabBar.js
+++ src/components/TabBar/tabBar.js
@@ -1,10 +1,15 @@
 import {Container, ContactButton, PostList } from './tabBar.Styled';
+import {MaterialIcons} from '@expo/vector-icons';
 
 export default function TabBar() {
   return(
     <Container>
-      <ContactButton></ContactButton>
-      <PostList></PostList>
+      <PostList>
+        <MaterialIcons name='message' size={30} color="#f39a4a"/>
+      </PostList>
+      <ContactButton>
+        <MaterialIcons name='reorder' size={30} color="#f39a4a"/>
+      </ContactButton>
     </Container>
   )
 }
